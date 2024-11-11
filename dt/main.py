602426b343791993ab730d09562688f026059a9a
Gaia Rossi <rssgai@unife.it>
2024-11-11 14:53:34 +0100

    Prometheus scraping endpoint and odte calculated on elements available if less than the size of window

--- dt/main.py
+++ dt/main.py
@@ -1,4 +1,7 @@
+from flask import Flask
 import paho.mqtt.client as mqtt, collections, json, time, threading
+
+app = Flask(__name__)
 
 messages_deque = collections.deque(maxlen=100)
 current_average = None
@@ -14,17 +17,19 @@ MQTT_TOPIC = "led_1"
 # WARN: lunghezza window = numero di elementi perche
 #       invia un aggiornamento al secondo
 
-def compute_timeliness(window_length_sec: int, desired_timeliness_sec: float) -> float:
+def compute_timeliness(desired_timeliness_sec: float) -> float:
     global observations
     obs_list = list(observations)
-    obs_required = obs_list[-window_length_sec:]
+
+    if len(obs_list) == 0:
+        return 0.0
 
     count = 0
-    for obs in obs_required:
+    for obs in obs_list:
         if obs <= desired_timeliness_sec:
             count += 1
 
-    percentile = float(count/window_length_sec)
+    percentile = float(count/len(obs_list))
 
     return percentile
 
@@ -36,12 +41,18 @@ def compute_reliability(window_length_sec: int, expected_msg_sec: int) -> float:
     msg_list = list(messages_deque)
     msg_required = msg_list[-window_length_sec:]
 
+    if len(msg_required) == 0:
+        return 0.0
+
     count = 0
     for msg in msg_required:
         if msg["timestamp"] >= start_window_time and msg["timestamp"] <= end_window_time:
             count += 1
 
-    expected_msg_tot = window_length_sec * expected_msg_sec
+    if window_length_sec >= len(msg_list):
+        expected_msg_tot = len(msg_list) * expected_msg_sec
+    else:
+        expected_msg_tot = window_length_sec * expected_msg_sec
 
     return float(count/expected_msg_tot)
 
@@ -49,7 +60,7 @@ def compute_availability() -> float:
     return 1.0
 
 def compute_odte_phytodig(window_length_sec, desired_timeliness_sec, expected_msg_sec):
-    timeliness = compute_timeliness(window_length_sec, desired_timeliness_sec)
+    timeliness = compute_timeliness(desired_timeliness_sec)
     reliability = compute_reliability(window_length_sec, expected_msg_sec)
     availability = compute_availability()
     print(f"Availability: {availability}\tReliability: {reliability}\tTimeliness: {timeliness}")
@@ -63,7 +74,7 @@ def on_message(client, userdata, message):
     global messages_deque, current_average, current_led_state, observations
     received_timestamp = time.time()
     start_exec_time = time.time()
-    print(f"Received message {message.payload} on topic {message.topic}")
+    # print(f"Received message {message.payload} on topic {message.topic}")
     data = json.loads(message.payload)
     messages_deque.append(data)
 
@@ -73,7 +84,7 @@ def on_message(client, userdata, message):
     for payload in messages_deque:
         tot += payload["power_consumption"]
     current_average = tot/len(messages_deque)
-    print(f"Current average is {current_average} and current state is {current_led_state}")
+    # print(f"Current average is {current_average} and current state is {current_led_state}")
 
     end_exec_time = time.time()
     execution_timestamp = end_exec_time - start_exec_time
@@ -84,11 +95,16 @@ def on_message(client, userdata, message):
     
 
 def odte_thread():
-    time.sleep(33)
     while True:
         computed_odte = compute_odte_phytodig(30, 0.5 ,1)
         print(f"ODTE computed {computed_odte}")   
         time.sleep(1)  
+
+@app.route("/metrics")
+def odte_prometheus():
+    odte = compute_odte_phytodig(30, 0.5 ,1)
+    prometheus_template = f"odte[pt=\"led_1\"] {str(odte)}".replace("[", "{").replace("]", "}")
+    return prometheus_template
 
 if __name__ == "__main__":
 
@@ -103,4 +119,6 @@ if __name__ == "__main__":
     mqtt_client.connect(MQTT_BROKER, MQTT_PORT)
     mqtt_client.subscribe(MQTT_TOPIC)
 
-    mqtt_client.loop_forever()
+    mqtt_client.loop_start()
+
+    app.run(host='0.0.0.0', port=8001)
