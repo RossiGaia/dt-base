78a45e951fb95e3eb01297ffc61c14f9bde6062e
Gaia Rossi <rssgai@unife.it>
2024-11-11 14:51:15 +0100

    First commit

---
+++ dt/main.py
@@ -0,0 +1,106 @@
+import paho.mqtt.client as mqtt, collections, json, time, threading
+
+messages_deque = collections.deque(maxlen=100)
+current_average = None
+current_led_state = None
+
+#odte
+observations = collections.deque(maxlen=100)
+
+MQTT_BROKER = "192.168.58.2"
+MQTT_PORT = 31095
+MQTT_TOPIC = "led_1"
+
+# WARN: lunghezza window = numero di elementi perche
+#       invia un aggiornamento al secondo
+
+def compute_timeliness(window_length_sec: int, desired_timeliness_sec: float) -> float:
+    global observations
+    obs_list = list(observations)
+    obs_required = obs_list[-window_length_sec:]
+
+    count = 0
+    for obs in obs_required:
+        if obs <= desired_timeliness_sec:
+            count += 1
+
+    percentile = float(count/window_length_sec)
+
+    return percentile
+
+def compute_reliability(window_length_sec: int, expected_msg_sec: int) -> float:
+    global messages_deque
+    end_window_time = time.time()
+    start_window_time = time.time() - window_length_sec
+
+    msg_list = list(messages_deque)
+    msg_required = msg_list[-window_length_sec:]
+
+    count = 0
+    for msg in msg_required:
+        if msg["timestamp"] >= start_window_time and msg["timestamp"] <= end_window_time:
+            count += 1
+
+    expected_msg_tot = window_length_sec * expected_msg_sec
+
+    return float(count/expected_msg_tot)
+
+def compute_availability() -> float:
+    return 1.0
+
+def compute_odte_phytodig(window_length_sec, desired_timeliness_sec, expected_msg_sec):
+    timeliness = compute_timeliness(window_length_sec, desired_timeliness_sec)
+    reliability = compute_reliability(window_length_sec, expected_msg_sec)
+    availability = compute_availability()
+    print(f"Availability: {availability}\tReliability: {reliability}\tTimeliness: {timeliness}")
+    return timeliness * reliability * availability
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    if reason_code == 0:
+        print(f"Connected to MQTT Broker at {MQTT_BROKER}")
+
+def on_message(client, userdata, message):
+    global messages_deque, current_average, current_led_state, observations
+    received_timestamp = time.time()
+    start_exec_time = time.time()
+    print(f"Received message {message.payload} on topic {message.topic}")
+    data = json.loads(message.payload)
+    messages_deque.append(data)
+
+    current_led_state = data["state"]
+
+    tot = 0
+    for payload in messages_deque:
+        tot += payload["power_consumption"]
+    current_average = tot/len(messages_deque)
+    print(f"Current average is {current_average} and current state is {current_led_state}")
+
+    end_exec_time = time.time()
+    execution_timestamp = end_exec_time - start_exec_time
+    message_timestamp = data["timestamp"]
+
+    # odte timeliness computation
+    observations.append(received_timestamp - message_timestamp + execution_timestamp)
+    
+
+def odte_thread():
+    time.sleep(33)
+    while True:
+        computed_odte = compute_odte_phytodig(30, 0.5 ,1)
+        print(f"ODTE computed {computed_odte}")   
+        time.sleep(1)  
+
+if __name__ == "__main__":
+
+    odte_t = threading.Thread(target=odte_thread, daemon=True)
+    odte_t.start()
+
+    mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+
+    mqtt_client.connect(MQTT_BROKER, MQTT_PORT)
+    mqtt_client.subscribe(MQTT_TOPIC)
+
+    mqtt_client.loop_forever()
---
+++ pt/main.py
@@ -0,0 +1,106 @@
+from flask import Flask
+from enum import Enum
+import random, time, threading, json, paho.mqtt.client as mqtt
+
+app = Flask(__name__)
+
+class LED_STATE(Enum):
+    ON = 1
+    OFF = 0
+
+class LED:
+    def __init__(self) -> None:
+        self._STATE = LED_STATE.OFF
+        self._POWER_CONSUMPTION  = 0
+        self._lock = threading.Lock()
+
+    def toggle(self):
+        with self._lock:
+            if self._STATE == LED_STATE.OFF:
+                self._STATE = LED_STATE.ON
+            else:
+                self._STATE = LED_STATE.OFF
+
+    def get_state(self):
+        with self._lock:
+            return self._STATE
+    
+    def get_power_consumption(self):
+        with self._lock:
+            return random.random() if self._STATE == LED_STATE.ON else 0.0
+    
+led = LED()
+
+# MQTT
+MQTT_BROKER = "192.168.58.2"
+MQTT_PORT = 31095
+MQTT_TOPIC = "led_1"
+
+mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    if reason_code == 0:
+        print(f"Connected to MQTT Broker at {MQTT_BROKER}")
+
+mqtt_client.on_connect = on_connect
+mqtt_client.connect(MQTT_BROKER, MQTT_PORT)
+
+@app.route("/power_consumption")
+def access_power_consumption():
+    global led
+    data = {
+        "value": led.get_power_consumption(),
+        "timestamp": time.time()
+    }
+    return json.dumps(data)
+
+@app.route("/led_state")
+def access_led_state():
+    global led
+    data = {
+        "value": led.get_state().name,
+        "timestamp": time.time()
+    }
+    return json.dumps(data)
+
+@app.route("/toggle", methods=["POST"])
+def toggle():
+    global led
+    led.toggle()
+    return json.dumps({
+        "message": "LED toggled",
+        "new_state": led.get_state().name
+        })
+
+
+def publish_to_mqtt_thread():
+    global led, mqtt_client
+
+    mqtt_client.loop_start()
+
+    while True:
+        data = {
+            "state": led.get_state().name,
+            "power_consumption": led.get_power_consumption(),
+            "timestamp": time.time()
+        }
+
+        mqtt_client.publish(MQTT_TOPIC, json.dumps(data))
+        print(f"Published message: {data}")
+        time.sleep(1)
+
+def toggle_thread():
+    global led
+    while True:
+        wait_time = random.randint(5, 30)
+        time.sleep(wait_time)
+        led.toggle()
+
+if __name__ == "__main__":
+    toggle_t = threading.Thread(target=toggle_thread, daemon=True)
+    toggle_t.start()
+
+    publish_t = threading.Thread(target=publish_to_mqtt_thread, daemon=True)
+    publish_t.start()
+
+    app.run(host='0.0.0.0', port=8000)
